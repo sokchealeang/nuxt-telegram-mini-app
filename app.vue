9dff84d50d5b4dbaa096eec0bcc77f02589a199c
Sokchea Leang <sokchealeang.dev@gmail.com>
2025-08-13 16:11:34 +0700

    chore: trying to get user data

--- app.vue
+++ app.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { initData } from '@telegram-apps/sdk-vue'
 
-const user = initData.user()
+const user = ref({})
+
+onMounted(() => {
+  user.value = initData.user || {}
+})
 </script>
 
 <template>
   <div>
-    <h2>Welcome, {{ user }} {{ user?.last_name }}</h2>
+    <h2>Welcome, {{ user }}</h2>
   </div>
 </template>
